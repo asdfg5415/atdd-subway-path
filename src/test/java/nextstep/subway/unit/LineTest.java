b790b77063e72d19386a7aa77ea424e2b3a41735
asdfg5415 <asdfg5415@gmail.com>
2022-07-27 02:23:55 +0900

    refactor: LineTest 공통 조건 수정

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -2,6 +2,7 @@ package nextstep.subway.unit;
 
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -11,35 +12,37 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 
 class LineTest {
 
+    Line line;
+    Station firstStation;
+    Station secondStation;
+    Station thirdStation;
+
+    @BeforeEach
+    void setUp() {
+        line = new Line();
+        firstStation = new Station("강남역");
+        secondStation = new Station("역삼역");
+        thirdStation = new Station("선릉역");
+    }
+
     @Test
     void addSection() {
-        Line line = new Line();
-        Station upStation = new Station("강남역");
-        Station downStation = new Station("역삼역");
-
-        line.addSection(upStation, downStation, 10);
+        line.addSection(firstStation, secondStation, 10);
 
         assertThat(line.getSections()).isNotEmpty();
     }
 
     @Test
     void getStations() {
-        Line line = new Line();
-        Station upStation = new Station("강남역");
-        Station downStation = new Station("역삼역");
-        line.addSection(upStation, downStation, 10);
+        line.addSection(firstStation, secondStation, 10);
 
         List<Station> stations = line.getStations();
 
-        assertThat(stations).containsExactly(upStation, downStation);
+        assertThat(stations).containsExactly(firstStation, secondStation);
     }
 
     @Test
     void removeSection() {
-        Line line = new Line();
-        Station firstStation = new Station("강남역");
-        Station secondStation = new Station("역삼역");
-        Station thirdStation = new Station("선릉역");
         line.addSection(firstStation, secondStation, 10);
         line.addSection(secondStation, thirdStation, 10);
 
@@ -50,10 +53,6 @@ class LineTest {
 
     @Test
     void removeSection_Fail() {
-        Line line = new Line();
-        Station firstStation = new Station("강남역");
-        Station secondStation = new Station("역삼역");
-        Station thirdStation = new Station("선릉역");
         line.addSection(firstStation, secondStation, 10);
         line.addSection(secondStation, thirdStation, 10);
 
